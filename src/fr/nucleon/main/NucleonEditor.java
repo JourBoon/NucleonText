20746a47100a334539eae89c79cf2232d5384f84
JourBoon <tom.caillaud85@gmail.com>
2020-10-30 15:37:18 +0100

    Ajout du backgroundcolor avec un borderPane

--- src/fr/nucleon/main/NucleonEditor.java
+++ src/fr/nucleon/main/NucleonEditor.java
@@ -1,14 +1,26 @@
 package fr.nucleon.main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 
 public class NucleonEditor extends Application {
     @Override
     public void start(Stage editor) throws Exception {
-        
-        editor.setTitle("NucleonText Editor");
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root);
+        editor.setTitle("NucleonText Editor V0.1.0");
+        editor.setScene(scene);
+        root.setStyle("-fx-background-color: #3A3A3A;");
+
+
+
+        root.getChildren().addAll();
         editor.show();
     }
 
